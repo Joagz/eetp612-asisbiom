62388f7801b7bf7ba4f85a308407d6a3c4df015f
Joagz <joagomez.dev@gmail.com>
2024-05-29 20:58:59 -0300

    creando listado de alumnos

--- asisbiom_admin_front/pages/alumnos/listado.tsx
+++ asisbiom_admin_front/pages/alumnos/listado.tsx
@@ -1,8 +1,163 @@
+import React, { useEffect, useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  TextField,
+  Select,
+  MenuItem,
+  Button,
+  Grid,
+} from "@mui/material";
+import { MainLayout } from "@/components";
 
-const listado = (/* Opciones de filtrado */) => {
+const Listado = () => {
+  // Ejemplo de datos
+  const [filtroNombre, setFiltroNombre] = useState("");
+  const [filtroCurso, setFiltroCurso] = useState("");
+  const [filtroDni, setFiltroDni] = useState("");
+  const [filtroDivision, setFiltroDivision] = useState("");
+
+  const datos = [
+    {
+      curso: "2",
+      division: "A",
+      nombre: "Juan Perez",
+      dni: "12345678",
+      diasHabiles: 60,
+      inasistencias: [2, 3, 1],
+    },
+    {
+      curso: "1",
+      division: "A",
+      nombre: "María Gomez",
+      dni: "87654321",
+      diasHabiles: 60,
+      inasistencias: [1, 2, 0],
+    },
+  ];
+  const [datosFiltrados, setDatosFiltrados] = useState<any>(datos);
+
+  function setFilter() {
+    setDatosFiltrados(
+      datos.filter(
+        (data) =>
+          data.nombre.toLowerCase().includes(filtroNombre.toLowerCase()) &&
+          data.curso.toLowerCase().includes(filtroCurso.toLowerCase()) &&
+          data.division.toLowerCase().includes(filtroDivision.toLowerCase()) &&
+          data.dni.toLowerCase().includes(filtroDni.toLowerCase())
+      )
+    );
+  }
+
   return (
-    <div>listado</div>
-  )
-}
+    <MainLayout title="Alumnos - Listado">
+      <article className="flex flex-col w-full">
+        <div className="h-20"></div>
+        <div className="flex justify-start gap-1 flex-wrap">
+          <TextField
+            className="flex-1 min-w-[200px]"
+            label="Filtrar por nombre"
+            variant="outlined"
+            value={filtroNombre}
+            onChange={(e) => setFiltroNombre(e.target.value)}
+            style={{ marginBottom: "10px", width: "300px" }}
+          />
+          <TextField
+            className="flex-1 min-w-[200px]"
+            label="Filtrar por DNI"
+            variant="outlined"
+            value={filtroDni}
+            onChange={(e) => setFiltroDni(e.target.value)}
+            style={{ marginBottom: "10px", width: "300px" }}
+          />
+          <Select
+            className="flex-1 min-w-[200px]"
+            label="Curso"
+            variant="outlined"
+            value={filtroCurso}
+            onChange={(e) => setFiltroCurso(e.target.value)}
+            style={{ marginBottom: "10px", width: "300px" }}
+          >
+            <MenuItem value={"1"}>1ero</MenuItem>
+            <MenuItem value={"2"}>2do</MenuItem>
+            <MenuItem value={"3"}>3ero</MenuItem>
+            <MenuItem value={"4"}>4to</MenuItem>
+            <MenuItem value={"5"}>5to</MenuItem>
+            <MenuItem value={"6"}>6to</MenuItem>
+          </Select>
+          <Select
+            className="flex-1 min-w-[200px]"
+            label="División"
+            variant="outlined"
+            value={filtroDivision}
+            onChange={(e) => setFiltroDivision(e.target.value)}
+            style={{ marginBottom: "10px", width: "300px" }}
+          >
+            <MenuItem value={"A"}>A</MenuItem>
+            <MenuItem value={"B"}>B</MenuItem>
+            <MenuItem value={"C"}>C</MenuItem>
+            <MenuItem value={"D"}>D</MenuItem>
+            <MenuItem value={"E"}>E</MenuItem>
+          </Select>
+          <Button
+            className="flex-1 h-[61.750px] min-w-[200px]"
+            onClick={() => setFilter()}
+            variant="contained"
+          >
+            Filtrar
+          </Button>{" "}
+        </div>
 
-export default listado
+        <TableContainer component={Paper} variant="outlined">
+          <Table size="small">
+            <TableHead>
+              <TableRow>
+                <TableCell className="font-bold">Curso</TableCell>
+                <TableCell className="font-bold">División</TableCell>
+                <TableCell className="font-bold">Nombre y Apellido/s</TableCell>
+                <TableCell className="font-bold">DNI</TableCell>
+                <TableCell className="font-bold">Días Hábiles</TableCell>
+                <TableCell className="font-bold" colSpan={3}>
+                  Inasistencias
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell>1er Trimestre</TableCell>
+                <TableCell>2do Trimestre</TableCell>
+                <TableCell>3er Trimestre</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {datosFiltrados.map((fila, index) => (
+                <TableRow key={index}>
+                  <TableCell>{fila.curso}</TableCell>{" "}
+                  {/* Reemplaza con el dato del curso */}
+                  <TableCell>{fila.division}</TableCell>{" "}
+                  {/* Reemplaza con el dato de la división */}
+                  <TableCell>{fila.nombre}</TableCell>
+                  <TableCell>{fila.dni}</TableCell>
+                  <TableCell>{fila.diasHabiles}</TableCell>
+                  <TableCell>{fila.inasistencias[0]}</TableCell>
+                  <TableCell>{fila.inasistencias[1]}</TableCell>
+                  <TableCell>{fila.inasistencias[2]}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </article>
+    </MainLayout>
+  );
+};
+
+export default Listado;
